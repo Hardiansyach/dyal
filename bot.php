c5a944167a453c15c7bd3ef6aa523af7f4550281
alroysh <cicak.jatuh02@gmail.com>
2018-03-04 03:00:32 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -123,106 +123,38 @@
 					}
 
 
-			// else if($pesan_datang==$datac){
-			// 	$api_ig = file_get_contents("https://www.instagram.com/".$data[1]."/?__a=1");
-			// 	$jss = json_decode($api_ig);
-			// 	$profile_pic_url_hd = $jss->user->profile_pic_url_hd;
-
-			// 	$text1 = 
-			// 	"Profil Instagram ".$data[1]."
-			// 	Username : ".$data[1]."
-			// 	Followers : ".$jss->user->followed_by->count."
-			// 	Following : ".$jss->user->follows->count."
-			// 	Post : ".$jss->user->media->count."
-			// 	Bio : ".$jss->user->biography."
-			// 	Website : ".$jss->user->external_url."
-			// 	Verified : ".$jss->user->is_verified."";
-
-
-			// 	$balas = array(
-			// 		'replyToken' => $replyToken,
-			// 		'messages' => array(
-			// 		array(
-			// 			'type' => 'text',
-			// 			'text' => $text1					
-			// 			),
-			// 		array(
-			// 			'type' => 'image',
-			// 			'originalContentUrl' => $profile_pic_url_hd,
-			// 			'previewImageUrl' => $profile_pic_url_hd, 					
-			// 			)
-			// 		)
-			// 	);				
-			// }
-
 			else if($pesan_datang==$datac){
 				$api_ig = file_get_contents("https://www.instagram.com/".$data[1]."/?__a=1");
 				$jss = json_decode($api_ig);
 				$profile_pic_url_hd = $jss->user->profile_pic_url_hd;
 
-				$text1 = "Followers : ".$jss->user->followed_by->count."";
+				$text1 = 
+				"Profil Instagram ".$data[1]."
+				Username : ".$data[1]."
+				Followers : ".$jss->user->followed_by->count."
+				Following : ".$jss->user->follows->count."
+				Post : ".$jss->user->media->count."
+				Bio : ".$jss->user->biography."
+				Website : ".$jss->user->external_url."
+				Verified : ".$jss->user->is_verified."";
 
 
 				$balas = array(
-		        'replyToken' => $replyToken,
-		        'messages' => array(
-		            array(
-		                'type' => 'template', // 訊息類型 (模板)
-		                'altText' => 'Profil Instagram', // 替代文字
-		                'template' => array(
-		                    'type' => 'carousel', // 類型 (旋轉木馬)
-		                    'columns' => array(
-		                        array(
-		                            'thumbnailImageUrl' => $profile_pic_url_hd, // 圖片網址 <不一定需要>
-		                            'title' => 'Instagram '.$data[1], // 標題 1 <不一定需要>
-		                            'text' => $text1, // 文字 1
-		                            'actions' => array(
-		                                array(
-		                                    'type' => 'postback', // 類型 (回傳)
-		                                    'label' => 'postback 1', // 標籤 1
-		                                    'data' => '/mulai-android-1' // 資料
-		                                ),
-		                                array(
-		                                    'type' => 'message', // 類型 (訊息)
-		                                    'label' => 'Message example 1', // 標籤 2
-		                                    'text' => 'Message example 1' // 用戶發送文字
-		                                ),
-		                                array(
-		                                    'type' => 'uri', // 類型 (連結)
-		                                    'label' => 'Uri example 1', // 標籤 3
-		                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
-		                                )
-		                            )
-		                        ),
-		                        array(
-		                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
-		                            'title' => 'Example Menu 2', // 標題 2 <不一定需要>
-		                            'text' => 'Description 2', // 文字 2
-		                            'actions' => array(
-		                                array(
-		                                    'type' => 'postback', // 類型 (回傳)
-		                                    'label' => 'postback 2', // 標籤 1
-		                                    'data' => 'hp' // 資料
-		                                ),
-		                                array(
-		                                    'type' => 'message', // 類型 (訊息)
-		                                    'label' => 'Message example 2', // 標籤 2
-		                                    'text' => 'Message example 2' // 用戶發送文字
-		                                ),
-		                                array(
-		                                    'type' => 'uri', // 類型 (連結)
-		                                    'label' => 'Uri example 2', // 標籤 3
-		                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
-		                                )
-		                            )
-		                        )
-		                    )
-		                )
-		            )
-		        )
-		    );
+					'replyToken' => $replyToken,
+					'messages' => array(
+					array(
+						'type' => 'text',
+						'text' => $text1					
+						),
+					array(
+						'type' => 'image',
+						'originalContentUrl' => $profile_pic_url_hd,
+						'previewImageUrl' => $profile_pic_url_hd, 					
+						)
+					)
+				);				
+			}
 
-	}
 
 			else if($pesan_datang=='modul'){
 				$balas = array(
