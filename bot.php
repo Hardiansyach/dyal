1044a09501cb7498f780f3372a5eb05b2797c12e
alroysh <cicak.jatuh02@gmail.com>
2018-03-03 22:06:28 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -456,7 +456,7 @@ else if($pesan_datang=='.example'){
                                 array(
                                     'type' => 'postback', // 類型 (回傳)
                                     'label' => 'postback 2', // 標籤 1
-                                    'data' => 'action=buy&itemid=123' // 資料
+                                    'data' => 'hp' // 資料
                                 ),
                                 array(
                                     'type' => 'message', // 類型 (訊息)
