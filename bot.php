2a7de781758895cf4484c7a04b979c49ec13ffaa
alroysh <cicak.jatuh02@gmail.com>
2018-03-03 10:50:19 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -209,86 +209,105 @@ else if($pesan_datang=='/about'){
 
 	}
 
-
-else if($pesan_datang=='/test'){
+else if($pesan_datang=='Android'){
 	$balas = array(
-		'replyToken' => $replyToken,
-		'messages' => array(
-			array(
-				'type' => 'template',
-				'altText' => 'Contoh carousel',
-				'template' =>[
-					'type' => 'carousel',
-					'columns' => [
-						[
-							'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
-							'imageBackgroundColor' => '#FFFFFF',
-							'title' => 'Hello',
-							'text' => 'Desc',
-							'defaultAction' => [
-									'type' => 'uri',
-									'label' => 'View',
-									'uri' => 'html.com',
-									'actions' => [
-										[
-										'type'=> 'postback',
-										'label'=> 'Buy',
-										'data'=> 'action=buy&itemid=111'
-										],
-										[
-										'type'=> 'postback',
-										'label'=> 'Buy',
-										'data'=> 'action=buy&itemid=111'
-										],
-										[
+				'replyToken' => $replyToken,														
+				'messages' => 
+				array(
+					 array(
+						   'type' => 'template',	
+						   'altText' => 'Android',
+						   'template' =>[
+						   		'type' => 'buttons',	
+						   		'thumbnailImageUrl' => 'https://s-media-cache-ak0.pinimg.com/600x315/9e/e4/a6/9ee4a64469336c1109775f11f25363ff.jpg',
+								'title' => 'Handphone Android',
+								'text' => 'Menu',
+								'actions' => [
+									[
+							    		'type' => 'uri',
+							    		'label' => 'Download Aplikasi',
+							    		'uri' => 'http://propanareload.com/propanareload.apk'
+									],
 
-										'type'=> 'postback',
-										'label'=> 'Buy',
-										'data'=> 'action=buy&itemid=111'										
-										]
-									] 
-							] 
-						],
-												[
-							'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
-							'imageBackgroundColor' => '#FFFFFF',
-							'title' => 'Hello',
-							'text' => 'Desc',
-							'defaultAction' => [
-									'type' => 'uri',
-									'label' => 'View',
-									'uri' => 'html.com',
-									'actions' => [
-										[
-										'type'=> 'postback',
-										'label'=> 'Buy',
-										'data'=> 'action=buy&itemid=111'
-										],
-										[
-										'type'=> 'postback',
-										'label'=> 'Buy',
-										'data'=> 'action=buy&itemid=111'
-										],
-										[
+									[
+							    		'type' => 'uri',
+							    		'label' => 'Follow Instagram',
+							    		'uri' => 'https://www.instagram.com/alroysh_/'
+									]	
+								]
+							]
+						)
+					)
+				);
 
-										'type'=> 'postback',
-										'label'=> 'Buy',
-										'data'=> 'action=buy&itemid=111'										
-										]
-									] 
-							] 
-						]
-					]
+	}
 
-				],
-				'imageAspectRatio'=> 'rectangle',
-				'imageSize'=> 'cover' 
-			)
-		)
-	);
-}
 
-else if($pesan_datang=='center'){
+else if($pesan_datang=='.example'){
+    $balas = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'template', // 訊息類型 (模板)
+                'altText' => 'Example buttons template', // 替代文字
+                'template' => array(
+                    'type' => 'carousel', // 類型 (旋轉木馬)
+                    'columns' => array(
+                        array(
+                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+                            'title' => 'Example Menu 1', // 標題 1 <不一定需要>
+                            'text' => 'Description 1', // 文字 1
+                            'actions' => array(
+                                array(
+                                    'type' => 'postback', // 類型 (回傳)
+                                    'label' => 'postback 1', // 標籤 1
+                                    'data' => 'action=buy&itemid=123' // 資料
+                                ),
+                                array(
+                                    'type' => 'message', // 類型 (訊息)
+                                    'label' => 'Message example 1', // 標籤 2
+                                    'text' => 'Message example 1' // 用戶發送文字
+                                ),
+                                array(
+                                    'type' => 'uri', // 類型 (連結)
+                                    'label' => 'Uri example 1', // 標籤 3
+                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                )
+                            )
+                        ),
+                        array(
+                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+                            'title' => 'Example Menu 2', // 標題 2 <不一定需要>
+                            'text' => 'Description 2', // 文字 2
+                            'actions' => array(
+                                array(
+                                    'type' => 'postback', // 類型 (回傳)
+                                    'label' => 'postback 2', // 標籤 1
+                                    'data' => 'action=buy&itemid=123' // 資料
+                                ),
+                                array(
+                                    'type' => 'message', // 類型 (訊息)
+                                    'label' => 'Message example 2', // 標籤 2
+                                    'text' => 'Message example 2' // 用戶發送文字
+                                ),
+                                array(
+                                    'type' => 'uri', // 類型 (連結)
+                                    'label' => 'Uri example 2', // 標籤 3
+                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    );
+
+	}
+
+
+
+else if($pesan_datang=='.example'){
     $balas = array(
         'replyToken' => $replyToken,
         'messages' => array(
