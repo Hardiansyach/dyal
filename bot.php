bee89d1799f584d2d6c6799b7a5a1d9a02709e75
alroysh <cicak.jatuh02@gmail.com>
2018-03-03 11:12:49 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -215,29 +215,29 @@ else if($pesan_datang=='android'){
         'messages' => array(
             array(
                 'type' => 'template', // 訊息類型 (模板)
-                'altText' => 'Android Device', // 替代文字
+                'altText' => 'Example buttons template', // 替代文字
                 'template' => array(
                     'type' => 'carousel', // 類型 (旋轉木馬)
                     'columns' => array(
                         array(
                             'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
-                            'title' => 'Aplikasi', // 標題 1 <不一定需要>
-                            'text' => 'Menu', // 文字 1
+                            'title' => 'Example Menu 1', // 標題 1 <不一定需要>
+                            'text' => 'Description 1', // 文字 1
                             'actions' => array(
                                 array(
-                                    'type' => 'uri', // 類型 (連結)
-                                    'label' => 'Download Aplikasi', // 標籤 3
-                                    'uri' => 'http://propanareload.com/propanareload.apk' // 連結網址
+                                    'type' => 'postback', // 類型 (回傳)
+                                    'label' => 'postback 1', // 標籤 1
+                                    'data' => 'action=buy&itemid=123' // 資料
+                                ),
+                                array(
+                                    'type' => 'message', // 類型 (訊息)
+                                    'label' => 'Message example 1', // 標籤 2
+                                    'text' => 'Message example 1' // 用戶發送文字
                                 ),
                                 array(
                                     'type' => 'uri', // 類型 (連結)
-                                    'label' => 'Download Aplikasi', // 標籤 3
-                                    'uri' => 'http://propanareload.com/propanareload.apk' // 連結網址
-                                ),
-                                array(
-                                    'type' => 'uri', // 類型 (連結)
-                                    'label' => 'Download Aplikasi', // 標籤 3
-                                    'uri' => 'http://propanareload.com/propanareload.apk' // 連結網址
+                                    'label' => 'Uri example 1', // 標籤 3
+                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
                                 )
                             )
                         ),
@@ -270,7 +270,6 @@ else if($pesan_datang=='android'){
     );
 
 	}
-
 
 
 
