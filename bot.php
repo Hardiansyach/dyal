1fe2c66c4f200d9f00896cb982c2cdaec9f5e978
alroysh <cicak.jatuh02@gmail.com>
2018-03-13 14:25:05 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -746,8 +746,14 @@
 				$content = file_get_contents($url);
 				$json = json_decode($content,true);
 				$judul = html_entity_decode($json['judul']);
+					
 				$no = 1;
-				
+				while($no <= count($json['detail'])){ 
+					$kode = $json['detail'][$no]['kode'];
+					$nama = $json['detail'][$no]['nama'];
+					$harga = $json['detail'][$no]['harga'];
+					$status = $json['detail'][$no]['status'];
+				   	
 				   	$balas = array(
 				   	    'replyToken' => $replyToken,
 				   	    'messages' => array(
@@ -757,45 +763,36 @@
 				   	            'template' => array(
 				   	                'type' => 'carousel', // 類型 (旋轉木馬)
 				   	                'columns' => array(
-				   	                    
+				   	                    array(
+				   	                        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/alroysh/dyal/master/image/photo4.jpg', // 圖片網址 <不一定需要>
+				   	                        'title' => $judul, // 標題 1 <不一定需要>
+				   	                        'text' => $nama, // 文字 1
+				   	                        'actions' => array(
+				   	                            array(
+				   	                                'type' => 'postback', // 類型 (回傳)
+				   	                                'label' => $kode, // 標籤 1
+				   	                                'data' => '/mulai-android-1' // 資料
+				   	                            ),
+				   	                            array(
+				   	                                'type' => 'postback', // 類型 (回傳)
+				   	                                'label' => $harga, // 標籤 1
+				   	                                'data' => '/mulai-android-1' // 資料
+				   	                            ),
+				   	                            array(
+				   	                                'type' => 'postback', // 類型 (回傳)
+				   	                                'label' => $status, // 標籤 1
+				   	                                'data' => '/mulai-android-1' // 資料
+				   	                            )
+				   	                        )
+				   	                    )
 				   	                )
 				   	            )
 				   	        )
 				   	    )
 				   	);
-
-
-							while($no <= count($json['detail'])){ 
-								$kode = $json['detail'][$no]['kode'];
-								$nama = $json['detail'][$no]['nama'];
-								$harga = $json['detail'][$no]['harga'];
-								$status = $json['detail'][$no]['status'];
-							   	$balas['messages']['template']['columns'][$no-1] = 
-							   	array(
-						   	                        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/alroysh/dyal/master/image/photo4.jpg', // 圖片網址 <不一定需要>
-						   	                        'title' => $judul, // 標題 1 <不一定需要>
-						   	                        'text' => $nama, // 文字 1
-						   	                        'actions' => array(
-						   	                            array(
-						   	                                'type' => 'postback', // 類型 (回傳)
-						   	                                'label' => $kode, // 標籤 1
-						   	                                'data' => '/mulai-android-1' // 資料
-						   	                            ),
-						   	                            array(
-						   	                                'type' => 'postback', // 類型 (回傳)
-						   	                                'label' => $harga, // 標籤 1
-						   	                                'data' => '/mulai-android-1' // 資料
-						   	                            ),
-						   	                            array(
-						   	                                'type' => 'postback', // 類型 (回傳)
-						   	                                'label' => $status, // 標籤 1
-						   	                                'data' => '/mulai-android-1' // 資料
-						   	                            )
-						   	                        )
-						   	                    );
-										$no++;
-						}
-	    
+								$no++;
+				}
+			    
 
 				}
 
