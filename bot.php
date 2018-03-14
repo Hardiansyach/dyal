4e7bf23e5362ade35d21ca8154b5e18c94666380
alroysh <cicak.jatuh02@gmail.com>
2018-03-14 18:27:32 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -767,67 +767,33 @@
 				$judul = html_entity_decode($json['judul']);
 				$no = 1;
 				$jumlahpesan = 1;
-				if(count($json['detail']) > 10){
-					//$jumlah = ceil(count($json['detail'])/10);
-					while($jumlahpesan <= 2){
-						$balas = array(
-                        'replyToken' => $replyToken,
-                        'messages' => array(
-                            array(
-                                'type' => 'template', // 訊息類型 (模板)
-                                'altText' => 'Harga '.$judul, // 替代文字
-                                'template' => array(
-                                    'type' => 'carousel', // 類型 (旋轉木馬)
-                                    'columns' =>  array()
-
-                                    )
-                                )
-                            )
-                        
-	                    );
 
 
-							while($no <= count($json['detail'])){ 
-								$kode = $json['detail'][$no]['kode'];
-								$nama = $json['detail'][$no]['nama'];
-								$harga = $json['detail'][$no]['harga'];
-								$status = $json['detail'][$no]['status'];
+				if(count($json['detail']) > 6){
 
-								if($status == "open"){
-									$status = "Tersedia";
-								}
-								else{
-									$status = "Gangguan";
-								}
+					$nom = 1;
+					$balas = array(
+					        'replyToken' => $replyToken,
+					        'messages' => array(
+					    	)
+					                        
+						 );
 
-								$bajing = 	array(
-						   	                        'thumbnailImageUrl' => 'https://raw.githubusercontent.com/alroysh/dyal/master/image/photo4.jpg', // 圖片網址 <不一定需要>
-						   	                        'title' => $judul, // 標題 1 <不一定需要>
-						   	                        'text' => $nama, // 文字 1
-						   	                        'actions' => array(
-						   	                            array(
-						   	                                'type' => 'postback', // 類型 (回傳)
-						   	                                'label' => "Kode : ".$kode, // 標籤 1
-						   	                                'data' => '/mulai-android-1' // 資料
-						   	                            ),
-						   	                            array(
-						   	                                'type' => 'postback', // 類型 (回傳)
-						   	                                'label' => "Harga : ".$harga, // 標籤 1
-						   	                                'data' => '/mulai-android-1' // 資料
-						   	                            ),
-						   	                            array(
-						   	                                'type' => 'postback', // 類型 (回傳)
-						   	                                'label' => "Status : ".$status, // 標籤 1
-						   	                                'data' => '/mulai-android-1' // 資料
-						   	                            )
-						   	                        )
-						   	                    );
-							   	array_push($balas['messages'][0]['template']['columns'], $bajing);
-							   $no++;
-						}
+					while($no <= 2){ 
+					$bajing = array(
+						'type' => 'text',
+						'text' => 're'					
+						);
+					   	array_push($balas['messages'], $bajing);
+					   $nom++;
 					}
 
-				}else{
+				}
+
+
+
+
+				else{
 
 					$no = 1;
 					$balas = array(
